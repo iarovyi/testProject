5cbf6c946f2d05afaec50e9b55c93cfe9b0b7f97
unknown <Sergii_Iarovyi@EPPLKRAW0161.budapest.epam.com>
2014-09-24 13:11:37 +0200

    first commit

---
+++ TeamCityConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityConsoleApplication
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
