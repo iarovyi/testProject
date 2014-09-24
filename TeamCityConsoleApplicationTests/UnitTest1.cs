07e6770d74c589ab51e071227d262b47c015c973
unknown <Sergii_Iarovyi@EPPLKRAW0161.budapest.epam.com>
2014-09-24 16:09:48 +0200

    add nunit tests

---
+++ TeamCityConsoleApplicationTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace TeamCityConsoleApplicationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, 10);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(15, 15);
+        }
+    }
+}
