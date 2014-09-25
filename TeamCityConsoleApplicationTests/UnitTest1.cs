3f20e403a634c097f8b916a86aaa7e7492e769d5
unknown <Sergii_Iarovyi@EPPLKRAW0161.budapest.epam.com>
2014-09-25 11:55:04 +0200

    dd

--- TeamCityConsoleApplicationTests/UnitTest1.cs
+++ TeamCityConsoleApplicationTests/UnitTest1.cs
@@ -11,14 +11,14 @@ namespace TeamCityConsoleApplicationTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(10, 11);
+            Assert.AreEqual(10, 10);
         }
 
         //[Test]
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(15, 16);
+            Assert.AreEqual(15, 15);
         }
     }
 }
