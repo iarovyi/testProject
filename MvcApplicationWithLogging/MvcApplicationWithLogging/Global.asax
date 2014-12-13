e0c551b7edc15ce85d329495e54dd8deebd481ff
Sergii Iarovyi <serge.zp.ua@gmail.com>
2014-12-13 09:09:37 +0100

    logger

---
+++ MvcApplicationWithLogging/MvcApplicationWithLogging/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApplicationWithLogging.MvcApplication" Language="C#" %>
---
+++ MvcApplicationWithLogging/MvcApplicationWithLogging/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Castle.Windsor;
+
+namespace MvcApplicationWithLogging
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static IWindsorContainer Container { get; private set; }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+
+
+            Container = new WindsorContainer();
+            Container.AddFacility<LoggingFacility>(f => f.UseNLog());
+
+            var logger = Container.Resolve<ILogger>();
+            logger.Error("Start up");
+        }
+    }
+}
